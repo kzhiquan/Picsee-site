ff6ce40e958e123490daac1d3fb4f938448e4961
kzhiquan <kzhiquan@gmail.com>
2019-06-30 14:44:23 +0800

    add scripts in package.json

--- .vuepress/dist/assets/js/9.5056d214.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{16:function(t,s,n){"use strict";n.r(s);var e=n(0),o=Object(e.a)({},function(){var t=this.$createElement,s=this._self._c||t;return s("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[s("p",[this._v("#post-2\n2222 post")])])},[],!1,null,null,null);s.default=o.exports}}]);
