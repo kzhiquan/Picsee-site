84e3e25829eb57d9447c5bbc27831b3c52f938c7
kzhiquan <kzhiquan@gmail.com>
2019-07-03 21:29:04 +0800

    Nav.vue components completed

--- .vuepress/theme/components/Footer.vue
+++ .vuepress/theme/components/Footer.vue
@@ -3,3 +3,14 @@
     <div class="text-grey-darker text-center">©2019 Yours truely. All rights reserved.</div>
   </footer>
 </template>
+
+<script>
+export default {
+  name: "Footer",
+  methods: {
+    formateDate(date) {
+      return moment(date).format("MMM Do YYYY")
+    },
+  },
+};
+</script>
